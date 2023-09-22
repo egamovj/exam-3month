fd2493fc42d4e7a975254de0a115a13c78f4c24f
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2023-09-22 23:54:05 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,1 @@
-var coll = document.getElementsByClassName('faqs__button')
-var i
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener('click', function() {
-        this.classList.toggle('active')
-        var content = this.nextElementSibling
-        if (content.style.display === 'block') {
-            content.style.display = 'none'
-        } else {
-            content.style.display = 'block'
-        }
-    })
-}
-
-document.querySelector('.header__burger').addEventListener('click', () => {
-    document.querySelector('.header__burger').classList.toggle('active')
-    document.querySelector('.sitenav').classList.toggle('active')
-})
